97a3422a578b4ada13bb866e9148eff179e14ddc
Célio Cidral Junior <ccidral@gmail.com>
2012-05-13 21:53:50 -0300

    Test menu item returned by the default plugin.

--- tomighty-swing/src/test/java/org/tomighty/plugin/DefaultPluginTest.java
+++ tomighty-swing/src/test/java/org/tomighty/plugin/DefaultPluginTest.java
@@ -3,27 +3,39 @@ package org.tomighty.plugin;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
 public class DefaultPluginTest {
 
-    //TODO test getPluginName
-    //TODO test getPluginVersion
-    //TODO test getMenuItem
+    private Plugin plugin;
+    private Injector injector;
 
-    @Test
-    public void testInjectorIsTheSameUsedToCreateThePlugin() {
-        Injector injector = Guice.createInjector(new AbstractModule() {
+    @Before
+    public void setUp() {
+        injector = Guice.createInjector(new AbstractModule() {
             @Override
             protected void configure() {
                 bind(Plugin.class).to(DefaultPlugin.class);
             }
         });
 
-        Plugin plugin = injector.getInstance(Plugin.class);
+        plugin = injector.getInstance(Plugin.class);
+    }
 
+    //TODO test getPluginName
+    //TODO test getPluginVersion
+
+    @Test
+    public void testHasNoMenuItem() {
+        assertNull(plugin.getMenuItem());
+    }
+
+    @Test
+    public void testInjectorIsTheSameUsedToCreateThePlugin() {
         assertSame(injector, plugin.getInjector());
     }
 
